423cbd7ffc956c0fa02802e2f5a61b099461bcf7
aljersulit <aljeromesulit@gmail.com>
2024-10-30 17:54:25 +0800

    finished exam

--- src/HiringTest.js
+++ src/HiringTest.js
@@ -1,4 +1,5 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 // Hi everyone, thank you for the interest in joining Supafaya.
 
@@ -10,7 +11,7 @@ import './App.css';
 
 // About the position:
 // The role/s we're hiring for is for Junior Web Developers. Our budget for the role is 19-25k/month + benefits + stock options. There will be no over time at any point during your employment in Supafaya. If ever there will be, it would be optional and paid.
-// Supafaya aims to be a $1Billion company in a few years and we would not be able to reach that if we don't do things differently. That said, we're implementing a strict Work From Home set up. 
+// Supafaya aims to be a $1Billion company in a few years and we would not be able to reach that if we don't do things differently. That said, we're implementing a strict Work From Home set up.
 // We're also doing something different in terms of work. Our work schedule is from 8AM - 5PM, 12-1PM lunch break. You'd be required to work on the features we have in the company for the first 6 hours, but from 3PM - 5PM you may work on any form of innovative feature you want (as long as it's related to the company). We want you to enjoy what you're doing and be curious. Work shouldn't be as draining as it is right now. It should be challenging and exciting.
 // As it is now, we're financially stable and have the capacity to run for two years without additional funding. However, we're looking to do a bigger raise in a few months. That said, we'd probably have a bigger teamm by then. We don't plan to hire Sr. Devs. Instead, we'll train you to become Sr. Devs. and have Jr. Devs. under you.
 
@@ -18,7 +19,7 @@ import './App.css';
 // Hi, I'm Owen! I'm the CEO and founder of Supafaya. I'm 24 years old. I'm a fullstack Software Engineer with background in Venture Capital, Finance, Data Science, and Law.
 // I would first like to thank you for the interest. If you're still here reading, then I've caught your interest. Thank you and I appreciate the time.
 // Second, I'd like to apologize since I won't be able to guide you for your whole stay here in Supafaya. I'd be here most of the time since I usually work 20hrs a day, but I can't promise to be there all the time since I would have to tend to other areas of the business.
-// That said, this role would really require a lot of self-learning and independent work. Every Tuesday and Thursday, 10am-11:30am, we'd hold our catch up meetings so that we could re-align on the timeline, the progress, and any roadblocks that you're facing. Other than that, I don't plan to breathe down your neck and check up on you all the time (unless its urgent). 
+// That said, this role would really require a lot of self-learning and independent work. Every Tuesday and Thursday, 10am-11:30am, we'd hold our catch up meetings so that we could re-align on the timeline, the progress, and any roadblocks that you're facing. Other than that, I don't plan to breathe down your neck and check up on you all the time (unless its urgent).
 // You would have complete freedom of the way you'd like to work, all I ask is that you attend our required meetings during Tuesdays and Thursdays, and deliver the work.
 // You guys would be our first hires. We don't have any Sr Devs nor do we have a CTO, and we don't plan to have any in the future. Our plan is to get a CTO and Sr. Devs. from this pool of applicants. The opportunity for growth here is absolutely unreal and I would love to have you guys in the company.
 // That said, good luck! And I'm looking forward to meet all of you.
@@ -53,73 +54,196 @@ import './App.css';
 // - The goal here is to replicate submitting a form.
 // - Input username, fullname, age then upon 'Submit' button click, the input fields should be reflected below.
 
-function HiringTest () {
-  return (
-    <div className="App">
-      <header className="App-header">
+function HiringTest() {
+	// PART 1
+	const [isVanished, setIsVanished] = useState(false);
 
-        {/* Challenge 1: Make the paragraph button vanish on click */}
-        <div>
-          <h1>1. Make this vanish</h1>
-          <button>Click me!</button>
-        </div>
+	// PART 2
+	const [timer, setTimer] = useState({
+		time: 0,
+		isRunning: false,
+	});
 
-        {/* Challenge 2: Make this timer work */}
-        <div>
-          <h1>2. Create a Basic Timer</h1>
-          <span> mins </span>
-          <span> secs</span>
-          <div className='button2'>
-            <button className='button green'>Start</button>
-            <button className='button red'>Stop</button>
-            <button className='button yellow'>Reset</button>
-          </div>
-        </div>
+	useEffect(() => {
+		let interval;
+		if (timer.isRunning) {
+			interval = setInterval(() => {
+				setTimer((prev) => {
+					return {
+						...prev,
+						time: prev.time++,
+					};
+				});
+			}, 1000);
+		} else if (!timer.isRunning && timer.time !== 0) {
+			clearInterval(interval);
+		}
 
-        {/* Challenge 3: Todo App */}
-        <div>
-          <h1>3. Add to a list</h1>
-          <input />
-          <button>Add Task</button>
-        </div>
-        
-        {/* Challenge 4: Submit a form */}
-        <div>
-          <h1>4. Submit a form</h1>
-          <form>
-            <div>
-              <label htmlFor="userName">
-                Username:
-                <input />
-              </label>
-            </div>
-            <br />
-            <div>
-              <label htmlFor="fullName">
-                FullName:
-                <input />
-              </label>
-            </div>
-            <br />
-            <div>
-              <label htmlFor="age">
-                Age:
-                <input />
-              </label>
-            </div>
-            <br />
-            <button>Submit</button>
-              <h4>Request Sent to DB with below request data</h4>
-              <ul>
-                <li>UserName: </li>
-                <li>FullName: </li>
-                <li>Age: </li>
-              </ul>
-          </form>
-        </div>
-      </header>
-    </div>
-  );
+		return () => clearInterval(interval);
+	}, [timer]);
+
+	// Start or stop timer
+	function handleTimer(type) {
+		setTimer((prev) => {
+			return {
+				...prev,
+				isRunning: { start: true, stop: false }[type],
+			};
+		});
+	}
+
+	// Reset timer
+	function resetTimer() {
+		setTimer({
+			time: 0,
+			isRunning: false,
+		});
+	}
+
+	// PART 3
+	const [todo, setTodo] = useState("");
+	const [todoList, setTodoList] = useState([]);
+
+	function handleInput(event) {
+		setTodo(event.target.value);
+	}
+
+	function addTodo() {
+		// Only add a todo if there is a value;
+		if (todo) {
+			setTodoList((prevList) => {
+				const list = [...prevList];
+				list.push(todo);
+				return list;
+			});
+		}
+	}
+
+	// PART 4
+	const [formData, setFormData] = useState({
+		userName: "",
+		fullName: "",
+		age: "",
+	});
+
+	const [formSubmitData, setFormSubmitData] = useState(null);
+
+	function formControl(event) {
+		const { name, value } = event.target;
+		setFormData((prev) => {
+			return {
+				...prev,
+				[name]: value,
+			};
+		});
+	}
+
+	function formSubmit(e) {
+		e.preventDefault();
+    setFormSubmitData(formData);
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				{/* Challenge 1: Make the paragraph button vanish on click */}
+				<div>
+					{!isVanished && <h1>1. Make this vanish</h1>}
+					<button onClick={() => setIsVanished((prev) => !prev)}>
+						Click me!
+					</button>
+				</div>
+
+				{/* Challenge 2: Make this timer work */}
+				<div>
+					<h1>2. Create a Basic Timer</h1>
+					<span> mins </span>
+					<span>{timer.time} secs</span>
+					<div className="button2">
+						<button
+							className="button green"
+							onClick={() => handleTimer("start")}
+						>
+							Start
+						</button>
+						<button
+							className="button red"
+							onClick={() => handleTimer("stop")}
+						>
+							Stop
+						</button>
+						<button className="button yellow" onClick={resetTimer}>
+							Reset
+						</button>
+					</div>
+				</div>
+
+				{/* Challenge 3: Todo App */}
+				<div>
+					<h1>3. Add to a list</h1>
+					<input name="todo" value={todo} onChange={handleInput} />
+					<button onClick={addTodo}>Add Task</button>
+					{todoList.map((task) => (
+						<p>{task}</p>
+					))}
+				</div>
+
+				{/* Challenge 4: Submit a form */}
+				<div>
+					<h1>4. Submit a form</h1>
+					<form onSubmit={formSubmit} method="POST">
+						<div>
+							<label htmlFor="userName">
+								Username:
+								<input
+									name="userName"
+									value={formData.userName}
+									onChange={formControl}
+								/>
+							</label>
+						</div>
+						<br />
+						<div>
+							<label htmlFor="fullName">
+								FullName:
+								<input
+									name="fullName"
+									value={formData.fullName}
+									onChange={formControl}
+								/>
+							</label>
+						</div>
+						<br />
+						<div>
+							<label htmlFor="age">
+								Age:
+								<input
+									name="age"
+									value={formData.age}
+									onChange={formControl}
+								/>
+							</label>
+						</div>
+						<br />
+						<button type="submit">Submit</button>
+						{formSubmitData && (
+							<>
+								<h4>
+									Request Sent to DB with below request data
+								</h4>
+								<ul>
+									<li>UserName: {formSubmitData.userName}</li>
+									<li>FullName: {formSubmitData.fullName}</li>
+									<li>Age: {formSubmitData.age}</li>
+								</ul>
+							</>
+						)}
+					</form>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default HiringTest;
+
